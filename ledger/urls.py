17d99ef3e1499d0598495cb75e21364cfc36eeb9
michellegillaineguzman <michelle.gillaine.guzman@student.ateneo.edu>
2026-02-19 18:41:25 +0800

    Lab 2: Models and ORM implementation

--- ledger/urls.py
+++ ledger/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('recipes/list', views.recipe_list, name='recipe_list'),
-    path('recipe/1', views.recipe_1, name='recipe_1'),
-    path('recipe/2', views.recipe_2, name='recipe_2'),
+    path('recipes/', views.recipe_list, name='recipe_list'),
+    path('recipe/<int:pk>/', views.recipe_detail, name='recipe_detail'),
 ]
--- ledger/views.py
+++ ledger/views.py
@@ -1,72 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Recipe
 
-# Create your views here.
+
 def recipe_list(request):
-    context = {
-        "recipes": [
-            {
-                "name": "Recipe 1",
-                "ingredients": [
-                    {"name": "tomato", "quantity": "3pcs"},
-                    {"name": "onion", "quantity": "1pc"},
-                    {"name": "pork", "quantity": "1kg"},
-                    {"name": "water", "quantity": "1L"},
-                    {"name": "sinigang mix", "quantity": "1 packet"},
-                ],
-                "link": "/recipe/1",
-            },
-            {
-                "name": "Recipe 2",
-                "ingredients": [
-                    {"name": "garlic", "quantity": "1 head"},
-                    {"name": "onion", "quantity": "1pc"},
-                    {"name": "vinegar", "quantity": "1/2cup"},
-                    {"name": "water", "quantity": "1 cup"},
-                    {"name": "salt", "quantity": "1 tablespoon"},
-                    {"name": "whole black peppers", "quantity": "1 tablespoon"},
-                    {"name": "pork", "quantity": "1 kilo"},
-                ],
-                "link": "/recipe/2",
-            },
-        ]
-    }
-
-    return render(request, "ledger/recipe_list.html", context)
+    recipes = Recipe.objects.all()
+    return render(request, "ledger/recipe_list.html", {"recipes": recipes})
 
 
-def recipe_1(request):
-    context = {
-        "recipe": {
-            "name": "Recipe 1",
-            "ingredients": [
-                {"name": "tomato", "quantity": "3pcs"},
-                {"name": "onion", "quantity": "1pc"},
-                {"name": "pork", "quantity": "1kg"},
-                {"name": "water", "quantity": "1L"},
-                {"name": "sinigang mix", "quantity": "1 packet"},
-            ],
-            "link": "/recipe/1",
-        }
-    }
-
-    return render(request, "ledger/recipe_detail.html", context)
-
-
-def recipe_2(request):
-    context = {
-        "recipe": {
-            "name": "Recipe 2",
-            "ingredients": [
-                {"name": "garlic", "quantity": "1 head"},
-                {"name": "onion", "quantity": "1pc"},
-                {"name": "vinegar", "quantity": "1/2cup"},
-                {"name": "water", "quantity": "1 cup"},
-                {"name": "salt", "quantity": "1 tablespoon"},
-                {"name": "whole black peppers", "quantity": "1 tablespoon"},
-                {"name": "pork", "quantity": "1 kilo"},
-            ],
-            "link": "/recipe/2",
-        }
-    }
-
-    return render(request, "ledger/recipe_detail.html", context)
+def recipe_detail(request, pk):
+    recipe = get_object_or_404(Recipe, pk=pk)
+    return render(request, "ledger/recipe_detail.html", {"recipe": recipe})
