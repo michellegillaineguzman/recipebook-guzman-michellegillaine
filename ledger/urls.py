a36d0afecb1430c519f5f5117fb281718feee109
michellegillaineguzman <michelle.gillaine.guzman@student.ateneo.edu>
2026-03-12 19:38:42 +0800

    Fix lab 3:  add password reset email template, add recipe list heading, fix recipe list URL

--- ledger/urls.py
+++ ledger/urls.py
@@ -4,6 +4,6 @@ from . import views
 app_name = 'ledger'
 
 urlpatterns = [
-    path('', views.recipe_list, name='recipe-list'),
-    path('<int:pk>/', views.recipe_detail, name='recipe-detail'),  
+    path('recipe/', views.recipe_list, name='recipe-list'),  
+    path('recipe/<int:pk>/', views.recipe_detail, name='recipe-detail'),
 ]
--- recipebook/settings.py
+++ recipebook/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'recipebook.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [], 
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
